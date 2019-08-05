1c37b4dc131ebca09f327063b039549e83e9abf5
jordiprats <jordi.prats@gmail.com>
2019-08-05 21:08:22 +0200

    dependencia epel

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,11 @@
-class openvpn(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits openvpn::params{
-
-  validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
+class openvpn (
+                $manage_package        = true,
+                $package_ensure        = 'installed',
+                $manage_service        = true,
+                $manage_docker_service = true,
+                $service_ensure        = 'running',
+                $service_enable        = true,
+              ) inherits openvpn::params{
 
   class { '::openvpn::install': }
   -> class { '::openvpn::config': }
--- manifests/params.pp
+++ manifests/params.pp
@@ -11,29 +11,9 @@ class openvpn::params {
       {
         /^[5-7].*$/:
         {
+          $include_epel=true
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
-      }
-    }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            /^16.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
       }
     }
     default: { fail('Unsupported OS!')  }
