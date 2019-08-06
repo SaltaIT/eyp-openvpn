4a85c08747ea7fdca1b29aae22962709a1ef0b23
jordiprats <jordi.prats@gmail.com>
2019-08-06 07:49:57 +0200

    config demo

--- examples/tuntcp.pp
+++ examples/tuntcp.pp
@@ -1,2 +1,14 @@
 openvpn::server { 'tuntcp':
+  proto => 'tcp',
+  client_to_client => true,
+  topology => 'subnet',
+  server => '172.16.102.0',
+  ifconfig_pool_persist_file => 'ipp.txt',
+  ping => '10',
+  ping_restart => '120',
+  push_ping => '10',
+  push_ping_restart => '60',
+  max_clients => '15',
+  persist_key => true,
+  persist_tun => true,
 }
