f09662984fb77569f7f9cb26522145afe41614e6
jordiprats <jordi.prats@gmail.com>
2019-09-29 18:47:21 +0200

    openvpn::server::ifconfigpush

---
+++ manifests/server/ifconfigpush.pp
@@ -0,0 +1,31 @@
+define openvpn::server::ifconfigpush(
+                                      $server_name,
+                                      $ipaddr,
+                                      $ccd     = 'ccd',
+                                      $fqdn    = $name,
+                                      $ensure  = 'present',
+                                      $netmask = '255.255.255.0',
+                                      $order   = '00',
+                                    ) {
+  include ::openvpn
+
+  if(!defined(Concat[]))
+  {
+    concat { "${openvpn::params::server_conf_dir}/${server_name}/${ccd}/${fqdn}":
+      ensure => $ensure,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      notify => Openvpn::Server::Service["openvpn-server@${server_name}"],
+    }
+  }
+
+  if($ensure=='present')
+  {
+    concat::fragment { "ifconfig push ${server_name} ${ccd} ${fqdn}":
+      target  => "${openvpn::params::server_conf_dir}/${server_name}/${ccd}/${fqdn}",
+      order   => $order,
+      content => template("${module_name}/server.erb"),
+    }
+  }
+}
